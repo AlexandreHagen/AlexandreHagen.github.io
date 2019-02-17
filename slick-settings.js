9d2a34d46c4786802375d5e36d458cf4558f4d28
Alexandre HAGEN <a.hagen@opendecide.com>
2019-02-17 15:34:08 +0100

    Init

---
+++ slick-settings.js
@@ -0,0 +1,41 @@
+$(function() {
+	$('.carousel').slick({
+		dots: false,
+		centerMode: true,
+		adaptiveHeight: false,
+		variableWidth: true,
+		infinite: true,
+		speed: 600,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 5000,
+		responsive: [
+		{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				infinite: true,
+				dots: false,
+				prevArrow: false,
+    			nextArrow: false,
+			}
+		},
+		{
+			breakpoint: 600,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+			}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}
+	    ]
+	});
+});
