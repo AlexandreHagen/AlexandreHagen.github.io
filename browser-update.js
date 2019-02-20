0e2c94efaeeaf7c47cc26b4b2f58f02a7b9b1417
Alexandre HAGEN <a.hagen@opendecide.com>
2019-02-20 17:39:12 +0100

    Improve
    Indentation
    Add youtube icon press page
    Add message to old browser
    Change email

---
+++ browser-update.js
@@ -0,0 +1,8 @@
+var $buoop = {required:{e:-6,f:-6,o:-6,s:-3,c:-6},api:2019.02}; 
+function $buo_f(){ 
+	var e = document.createElement("script"); 
+	e.src = "//browser-update.org/update.min.js"; 
+	document.body.appendChild(e);
+};
+try {document.addEventListener("DOMContentLoaded", $buo_f,false)}
+catch(e){window.attachEvent("onload", $buo_f)}
